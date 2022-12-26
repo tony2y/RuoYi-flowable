59935dd2f2503bc53ea87b00bca94fa537affc29
tony <846249920@qq.com>
2022-12-26 11:37:28 +0800

    feat: 更新流程表相关ID生成规则

--- ruoyi-flowable/src/main/java/com/ruoyi/flowable/config/FlowableConfig.java
+++ ruoyi-flowable/src/main/java/com/ruoyi/flowable/config/FlowableConfig.java
@@ -1,23 +1,23 @@
-package com.ruoyi.flowable.flow;
+package com.ruoyi.flowable.config;
 
+import org.flowable.engine.impl.db.DbIdGenerator;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.flowable.spring.boot.EngineConfigurationConfigurer;
 import org.springframework.context.annotation.Configuration;
 
-
 /**
+ * 流程id生成处理
  * @author XuanXuan
- * @date 2021/4/5 01:32
+ * @date 2022-12-26 10:24
  */
 @Configuration
 public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
-
     @Override
     public void configure(SpringProcessEngineConfiguration engineConfiguration) {
         engineConfiguration.setActivityFontName("宋体");
         engineConfiguration.setLabelFontName("宋体");
         engineConfiguration.setAnnotationFontName("宋体");
-
+        engineConfiguration.setIdGenerator(new DbIdGenerator());
     }
-}
 
+}
