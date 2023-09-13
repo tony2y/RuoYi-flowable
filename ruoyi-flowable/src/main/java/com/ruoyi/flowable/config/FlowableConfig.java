7f14a9a5014734581cc5ffb32c386a29e9f874b5
tony <846249920@qq.com>
2023-09-13 08:01:39 +0800

    fix: 修复启动错误问题

--- ruoyi-flowable/src/main/java/com/ruoyi/flowable/config/FlowableConfig.java
+++ ruoyi-flowable/src/main/java/com/ruoyi/flowable/config/FlowableConfig.java
@@ -3,7 +3,10 @@ package com.ruoyi.flowable.config;
 import org.flowable.engine.impl.db.DbIdGenerator;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 /**
  * 流程id生成处理
@@ -12,6 +15,12 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
+
+    @Bean
+    public AsyncListenableTaskExecutor applicationTaskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+
     @Override
     public void configure(SpringProcessEngineConfiguration engineConfiguration) {
         engineConfiguration.setActivityFontName("宋体");
